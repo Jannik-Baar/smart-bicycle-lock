55636987d19bdf86c6b0d168a51c79aadc2776c1
Oliver Kuhn <110844181+acariaC@users.noreply.github.com>
2023-06-06 22:00:11 +0200

    iOS app offering basic UI functionality

---
+++ sLock/sLock/sLock/sLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  sLockApp.swift
+//  sLock
+//
+//  Created by Oliver Kuhn on 06.06.23.
+//
+
+import SwiftUI
+
+@main
+struct sLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
