c718a59d31408a38afec5bdb83fdc77596fb7fd0
Jannik-Baar <jannik.ba99@gmail.com>
2023-06-23 20:42:22 +0200

    Update App

--- web-app/src/NFC.js
+++ web-app/src/NFC.js
@@ -5,7 +5,10 @@ import Alert from "@mui/material/Alert";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+import Fab from "@mui/material/Fab";
+import KeyIcon from '@mui/icons-material/Key';
+import Box from '@mui/material/Box'
 
 export default function NFC() {
     const [error, setError] = useState(null);
@@ -19,7 +22,7 @@ export default function NFC() {
 
         setShowProgress(true);
         try {
-            await ndef.write("Hello world!", {signal: controller.signal});
+            await ndef.write("", {signal: controller.signal});
         } catch (error) {
             switch (error.name) {
                 case "NotAllowedError":
@@ -65,10 +68,29 @@ export default function NFC() {
                     {error}
                 </Alert>
             </Collapse>
-            <Stack mt={5} direction="column" alignItems="center" justifyContent="center">
-                <Button variant="contained" onClick={() => writeMessage()} disabled={showProgress}>(un)lock</Button>
-            </Stack>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Stack mt={20} direction="column" alignItems="center" justifyContent="center" sx={{ position: "relative" }}>
+                    <Fab
+                        aria-label="save"
+                        color="primary"
+                        onClick={writeMessage}
+                        disabled={showProgress}
+                    >
+                        <KeyIcon/>
+                    </Fab>
+                    {showProgress && (
+                        <CircularProgress
+                            size={68}
+                            sx={{
+                                position: 'absolute',
+                                top: -6,
+                                left: -6,
+                                zIndex: 1,
+                            }}
+                        />
+                    )}
+                </Stack>
+            </Box>
         </>
     );
-
 }
